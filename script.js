b140b893c53e58e8c07471dcdf65a4d8690a3538
Anirudh Prakash <anipra7@gmail.com>
2019-11-21 19:29:51 -0800

    Basically finished weather app
    added lot more functionality

--- script.js
+++ script.js
@@ -1,7 +1,98 @@
+window.addEventListener("load", init)
+
+let BaseAPI = "https://api.openweathermap.org/data/2.5/weather?";
+let APIKey = "7b9ef6d5a3c36d00b45d1c53aa1413c9";
+
 function FtoC(degree) {
-    return (5 / 9) * (degree - 32);
+    return Math.round((5 / 9) * (degree - 32));
 }
 
 function CtoF(degree) {
-    return (9 / 5) * degree + 32;
+    return Math.round((9 / 5) * degree + 32);
+}
+
+function init() {
+    let buttonC = document.getElementById("C");
+    let buttonF = document.getElementById("F");
+    buttonC.disabled = true;
+
+    buttonC.addEventListener("click", function(){
+        let temp = document.querySelector("h1");
+        let temp_num = parseInt(temp.innerText);
+        temp.innerText = FtoC(temp_num) + decodeURI("%C2%B0");
+        buttonC.disabled = true;
+        buttonF.disabled = false;
+    })
+
+    buttonF.addEventListener("click", function(){
+        let temp = document.querySelector("h1");
+        let temp_num = parseInt(temp.innerText);
+        temp.innerText = CtoF(temp_num) + decodeURI("%C2%B0");
+        buttonF.disabled = true;
+        buttonC.disabled = false;
+    })
+
+    function changeImage(new_img) {
+        let image = document.getElementById("img");
+        console.log(image);
+        let imgs = ["cloudy", "rain", "semi_cloudy", "snowflake", "storm", "sunny"];
+        if (!imgs.includes(new_img.toLowerCase())) {
+            image.src = "images/cloudy.svg";
+        } else {
+            image.src = "images/" + new_img + ".svg";
+        }
+    }
+
+    changeLocation();
+    let buttonZip = document.getElementById("zip_btn");
+    let buttonCity = document.getElementById("city_btn");
+
+    buttonZip.addEventListener("click", function() {
+        changeLocation("zip");
+    });
+
+    buttonCity.addEventListener("click", function() {
+        changeLocation("city");
+    });
+
+    function changeLocation(mode) {
+        let units = "metric";
+        if (buttonF.disabled) {
+            units = "imperial";
+        }
+        let key = "q=Seattle";
+        if (mode != undefined)
+            key = ((mode == "city") ? "q" : mode) + "=" + document.getElementById("get_" + mode).value;
+        console.log(key);
+
+        let url = BaseAPI + key + "&units=" + units + "&appid=" + APIKey;
+        console.log(url);
+        fetch(url)
+            .then(function(data) {
+                if (data.status >= 200 && data.status < 300) {
+                    return data.text();
+                } else {
+                    return 0;
+                }
+            })
+            .then(JSON.parse)
+            .then(function(data) {
+                console.log(data);
+                let temp = document.querySelector("h1");
+                temp.innerText = Math.round(data.main.temp) + decodeURI("%C2%B0");
+                
+                let cast = document.querySelector("h4");
+                cast.innerText = data.weather[0].main;
+                changeImage(data.weather[0].main);
+                
+                let place = document.querySelector("h3");
+                place.innerText = data.name;
+
+                let country = document.querySelector("h5");
+                country.innerText = data.sys.country;
+            })
+            .catch(function() {
+                alert("Wrong Request");
+            });
+    }
 }
